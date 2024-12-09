72650e3525e95bd3683047241c4a7475dcd6c389
401-Nick <nick.larue17@gmail.com>
2024-12-08 21:40:55 -0600

    fixed env var names

--- app/utils/verify_env.py
+++ app/utils/verify_env.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 def check_env_variables_exist():
     load_dotenv()
-    if os.getenv("Secret_Key") is None or os.getenv("DATABASE_URL") is None:
+    if os.getenv("SECRET_KEY") is None or os.getenv("DATABASE_URI") is None:
         raise ValueError(
             "No environment variables found. Please create a .env file in the root directory. Travel-Metric-Dashboard/.env and add the following variables: SECRET_KEY, DATABASE_URI"
         )
